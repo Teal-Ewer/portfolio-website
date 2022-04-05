e1c0abd66c8efa4f8583fd271b108fb837902f5c
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-04-05 09:37:52 -0400

    update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,11 @@ const colors = require("tailwindcss/colors");
 module.exports = {
 	mode: "jit",
 	purge: ["./public/**/*.html"],
-	darkMode: false, // or 'media' or 'class'
+	darkMode: "media", // or 'media' or 'class'
 	theme: {
 		extend: {
 			colors: {
-				primary: "#005666",
-				teal: colors.teal,
-				cyan: colors.cyan,
+				primary: "#005666"
 			},
 		},
 	},
